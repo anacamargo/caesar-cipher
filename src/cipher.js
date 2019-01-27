3add80ed92b9848d8f64878a22ee2b7dc5a06cf6
anacamargo <anaclaudia2501@gmail.com>
2019-01-27 00:48:01 -0200

    Implementação do encode e decode

---
+++ src/cipher.js
@@ -0,0 +1,47 @@
+// O módulo no javaScriptvfunciona como o resto da divisão e não como módulo.
+// https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers
+
+function mod(n,m){
+    return ((n % m) + m) % m;
+}
+
+function cipherClick(){
+    const offset = parseInt(document.getElementById("offset").value);
+    const str = document.getElementById("cipher").value;
+    document.getElementById("cipher").value = "";
+    const result = encode(offset, str);
+    console.log({offset, str, result});
+    document.getElementById("decipher").value = result;
+}
+
+function decipherClick(){
+    const offset = parseInt(document.getElementById("offset").value);
+    const str = document.getElementById("decipher").value;
+    const result = decode(offset, str);
+    console.log({offset, str, result});    
+    document.getElementById("cipher").value = result;
+}
+
+function encode(offset, str){
+    let encrypted = "";
+    for(let i = 0; i < str.length; i++){
+        const code = str.charCodeAt([i]);
+        //const newCode = ((code - 32) + offset) % 95 + 32;
+        const newCode = mod((code - 32) + offset, 95) + 32;
+        const newLetter = String.fromCharCode(newCode);
+        encrypted += newLetter;
+    }
+    return encrypted;              
+}
+
+function decode(offset, str){
+    let decrypted = "";
+    for(let i = 0; i < str.length; i++){
+        let code = str.charCodeAt([i]);
+        //let newCode = ((code - 32) - offset) % 95 + 32;
+        const newCode = mod((code - 32) - offset, 95) + 32;
+        let newLetter = String.fromCharCode(newCode);
+        decrypted += newLetter;
+    }
+    return decrypted;
+}
