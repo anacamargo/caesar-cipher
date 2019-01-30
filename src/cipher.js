a1f238ca50a82f27f049e6788b03abfbbb582dfd
anacamargo <anaclaudia2501@gmail.com>
2019-01-30 19:49:41 -0200

    Inclusão de CSS

--- src/cipher.js
+++ src/cipher.js
@@ -1,47 +1,51 @@
-// O módulo no javaScriptvfunciona como o resto da divisão e não como módulo.
+// O módulo no javaScriptv funciona como o resto da divisão e não como módulo.
 // https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers
 
-function mod(n,m){
+function mod(n, m) {
     return ((n % m) + m) % m;
 }
 
-function cipherClick(){
+function cipherClick() {
     const offset = parseInt(document.getElementById("offset").value);
+    if (isNaN(offset)) {
+        return message = document.getElementById("offset").value = "Por favor digite um número";
+    }
     const str = document.getElementById("cipher").value;
     document.getElementById("cipher").value = "";
     const result = encode(offset, str);
-    console.log({offset, str, result});
     document.getElementById("decipher").value = result;
 }
 
-function decipherClick(){
+function decipherClick() {
     const offset = parseInt(document.getElementById("offset").value);
+    if (isNaN(offset)) {
+        return message = document.getElementById("offset").value = "Por favor digite um número";
+    }
     const str = document.getElementById("decipher").value;
     const result = decode(offset, str);
-    console.log({offset, str, result});    
     document.getElementById("cipher").value = result;
 }
 
-function encode(offset, str){
+function encode(offset, str) {
     let encrypted = "";
-    for(let i = 0; i < str.length; i++){
+    for (let i = 0; i < str.length; i++) {
         const code = str.charCodeAt([i]);
         //const newCode = ((code - 32) + offset) % 95 + 32;
         const newCode = mod((code - 32) + offset, 224) + 32;
         const newLetter = String.fromCharCode(newCode);
         encrypted += newLetter;
     }
-    return encrypted;              
+    return encrypted;
 }
 
-function decode(offset, str){
+function decode(offset, str) {
     let decrypted = "";
-    for(let i = 0; i < str.length; i++){
+    for (let i = 0; i < str.length; i++) {
         let code = str.charCodeAt([i]);
-        //let newCode = ((code - 32) - offset) % 95 + 32;
         const newCode = mod((code - 32) - offset, 224) + 32;
         let newLetter = String.fromCharCode(newCode);
         decrypted += newLetter;
     }
+
     return decrypted;
 }
---
+++ src/tests.js
@@ -0,0 +1,11 @@
+
+
+var result = encode(2 , 'aba');
+
+ if (result === "cdc"){
+    console.log('Deu certo');
+ }else console.log('Não funcionou');
+
+
+ 
+
